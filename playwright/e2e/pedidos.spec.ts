17f43219e04a5bfbb75494d815bafbc4df261e2b
Joseilton Correia <joseilton@aloware.com>
2026-06-11 14:37:57 -0300

    AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,20 +1,19 @@
 import { test, expect } from '@playwright/test';
 
+// AAA - Arrange, Act, Assert
+
 test('deve consultar um pedido aprovado', async ({ page }) => {
+  // Arrange
   await page.goto('http://localhost:5173/');
-
-  // Checkpoint 1: Verificar se a página de landing está online
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
-
   await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-
-  // Checkpoint 2: Verificar se a página de consulta de pedido está online
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
+  // Act
   await page.getByTestId('search-order-id').fill('VLO-MQ2U1V');
   await page.getByTestId('search-order-button').click();
 
-  // Checkpoint 3: Verificar se o pedido foi encontrado
+  // Assert
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-MQ2U1V');
   await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
 });
